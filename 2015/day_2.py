dc3b0ff10e31ecaf3190145bf80352c457a3e77b
Ian Thomas <i.c.thomas@gmail.com>
2022-11-24 17:40:16 +0100

    Added day_2

--- 2015/day_2.py
+++ 2015/day_2.py
@@ -1,20 +1,23 @@
 # read dimensions
 with open("./data/input_2.txt", "r") as f:
     lines = f.readlines()
-    dimensions = [list(map(int, entry.strip().split('x'))) for entry in lines]
+    dimensions = [sorted(map(int, entry.strip().split('x'))) for entry in lines]
 
 
-def calculate_area(d: list) -> int:
-    a = d[0] * d[1]
-    b = d[1] * d[2]
-    c = d[0] * d[2]
-    return ((a + b + c) * 2) + min(a, b, c)
+def calculate_paper(d: list) -> int:
+    return (2 * d[0] * d[1]) + (2 * d[0] * d[2]) + (2 * d[1] * d[2]) + (d[0] * d[1])
+
+
+def calculate_ribbon(d: list) -> int:
+    return (2 * d[0]) + (2 * d[1]) + (d[0] * d[1] * d[2])
 
 
 area = 0
+ribbon = 0
 
-# part 1
+# part 1 + 2
 for dimension in dimensions:
-    area += calculate_area(dimension)
+    area += calculate_paper(dimension)
+    ribbon += calculate_ribbon(dimension)
 
-print(f'Total area required: {area}')
+print(f'Total paper: {area}.  Total ribbon: {ribbon}')
